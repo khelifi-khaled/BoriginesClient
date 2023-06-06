d418bd185127d426dad226a63ae820b9d4d2e617
khelifi <khelifi.khaled55@gmail.com>
2023-06-06 13:43:40 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `<div>{{message}}</div>`,
 })
 export class AppComponent {
   title = 'borigines';
+  message = '';
 
+  constructor(private http: HttpClient) {
+    this.http.get('/api/message')
+      .subscribe((resp: any) => this.message = resp.text);
+  }
   
 }
